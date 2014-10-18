a97735c771b1ff47d54443df525fa610b7b47905
Rayan Bregigeon <rayan.bregigeon@epita.fr>
2014-10-18 15:34:08 +0200

    binarisation

--- OCR.c
+++ OCR.c
@@ -3,7 +3,7 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <err.h>
-
+#include "pixel_operation.h"
 
 
 void wait_for_keypressed(void) {
@@ -17,8 +17,75 @@ void wait_for_keypressed(void) {
   }
 }
 
-void detect_ligne(void) {
-  
+SDL_Surface*  binarize(SDL_Surface *s) {
+  Uint8 r, g, b;
+  double grey;
+  for(int i = 0; i < s->w; ++i)
+  {
+    for(int j = 0; j < s->h; ++j)
+    {
+      Uint32 pixel = getpixel(s,i,j);
+      SDL_GetRGB(pixel, s->format, &r, &g, &b);
+      grey = (double)r*0.3 + (double)g*0.59+(double)b*0.11;
+      if (grey<255/2)
+        pixel = SDL_MapRGB(s->format, 0, 0, 0);
+      else
+        pixel = SDL_MapRGB(s->format,255,255,255);
+      putpixel(s, i, j, pixel);
+    }
+  }
+  return s;
+}
+
+
+static inline
+Uint8* pixelref(SDL_Surface *surf, unsigned x, unsigned y) {
+  int bpp = surf->format->BytesPerPixel;
+  return (Uint8*)surf->pixels + y * surf->pitch + x * bpp;
+}
+ 
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    return *p;
+  case 2:
+    return *(Uint16 *)p;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+      return p[0] << 16 | p[1] << 8 | p[2];
+    else
+      return p[0] | p[1] << 8 | p[2] << 16;
+  case 4:
+    return *(Uint32 *)p;
+  }
+  return 0;
+}
+ 
+void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    *p = pixel;
+    break;
+  case 2:
+    *(Uint16 *)p = pixel;
+    break;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+      p[0] = (pixel >> 16) & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = pixel & 0xff;
+    } else {
+      p[0] = pixel & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = (pixel >> 16) & 0xff;
+    }
+    break;
+  case 4:
+    *(Uint32 *)p = pixel;
+    break;
+  }
 }
 
 
@@ -54,6 +121,8 @@ SDL_Surface* display_image(SDL_Surface *img) {
 int main()
 {
    init_sdl();
-   display_image(load_image("test_ocr.jpg"));
+   SDL_Surface *img = load_image("test_ocrbite.jpg");
+   display_image(img);
+   display_image(binarize(img));
    return 0;
 }
