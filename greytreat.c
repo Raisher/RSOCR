a96564535586ff986c6074dcd1a14ccca246a0e6
Rayan Bregigeon <rayan.bregigeon@epita.fr>
2014-12-07 12:53:06 +0100

    niveau de gris

---
+++ greytreat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pixel_operation.h"
+#include "greytreat.h"
+
+void toGrayLevel(SDL_Surface *s)
+{
+	int i;
+	int j;
+	Uint32 grey;
+	Uint8 r,g,b;
+	for(i = 0; i < s->w; i++)
+		for(j = 0; j < s->h; j++)
+		{
+			SDL_GetRGB(getpixel(s,i,j), s->format, &r, &g, &b);
+			grey =((Uint32)r + (Uint32)g + (Uint32) b)/3;
+			Uint32 pixel = SDL_MapRGB (s -> format, grey, grey, grey);
+			putpixel(s, i,j, pixel);
+		}
+}
+		       	
---
+++ greytreat.h
@@ -0,0 +1,6 @@
+#ifndef GREYTREAT_H
+#define GREYTREAT_H
+
+void toGrayLevel (SDL_Surface *s);
+
+#endif
